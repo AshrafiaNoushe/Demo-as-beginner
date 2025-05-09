24cffc85201376133047c03c5cf832eb3c80e9bd
Ashrafia Noushe <ashrafianoushe779@gmail.com>
2025-05-09 10:42:36 +0600

    GCD of Two num

---
+++ gcd_common.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+int gcd(int a,int b){
+    int temp;
+    if(a<b){
+        while(a!=0){
+            temp = a;
+            a = b%a;
+            b = temp;
+        }
+        return b;
+    }else{
+        if(a>b){
+            while(b!=0){
+                temp = b;
+                b = a%b;
+                a = temp;
+            }
+        }
+        return b;       
+    }
+}
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    gcd(a,b);
+    int gcd_num = gcd(a,b);
+    cout<<"GCD: "<<gcd_num<<endl;
+    return 0;
+}
